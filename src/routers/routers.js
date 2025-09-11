24646732e1783a29b7294b0389cefa1652da3c87
Danil <126762728+Migetsu@users.noreply.github.com>
2025-09-11 14:27:19 +0500

    Refactor router to use dynamic imports

--- src/routers/routers.js
+++ src/routers/routers.js
@@ -1,21 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '@/components/Home/Home.vue'
-import Anime from '@/components/Animes/Animes.vue'
-import Releases from '@/components/Releases/Releases.vue'
-import Ongoings from '@/components/Ongoings/Ongoings.vue'
-import Populars from '@/components/Popular/Populars.vue'
-import Watch from '@/components/Watch/Watch.vue'
-import Profile from '@/components/Profile/Profile.vue'
 
 export const routers = createRouter({
     history: createWebHistory(),
     routes: [
-        {path: '/', name: 'home', component: Home},
-        { path: '/animes/:id', name: 'animes', component: Anime, props: true },
-        { path: '/watch/:id', name: 'watch', component: Watch, props: true },
-        { path: '/releases', name: 'releases', component: Releases, props: true },
-        { path: '/ongoings', name: 'ongoings', component: Ongoings, props: true },
-        { path: '/populars', name: 'populars', component: Populars, props: true },
-        { path: '/profile', name: 'profile', component: Profile },
+        { path: '/', name: 'home', component: () => import('@/components/Home/Home.vue') },
+        { path: '/animes/:id', name: 'animes', component: () => import('@/components/Animes/Animes.vue'), props: true },
+        { path: '/watch/:id', name: 'watch', component: () => import('@/components/Watch/Watch.vue'), props: true },
+        { path: '/releases', name: 'releases', component: () => import('@/components/Releases/Releases.vue'), props: true },
+        { path: '/ongoings', name: 'ongoings', component: () => import('@/components/Ongoings/Ongoings.vue'), props: true },
+        { path: '/populars', name: 'populars', component: () => import('@/components/Popular/Populars.vue'), props: true },
+        { path: '/profile', name: 'profile', component: () => import('@/components/Profile/Profile.vue') },
     ]
 })
